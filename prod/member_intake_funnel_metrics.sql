4dbda4c6df47e2b1ac093ef4ac7712e522e77b72
kpook <kelly.pook@wellthy.com>
2024-03-08 15:13:10 -0600

    add new prod model, update docs

--- prod/member_intake_funnel_metrics.sql
+++ prod/member_intake_funnel_metrics.sql
@@ -1,17 +1,21 @@
 WITH funnel_events AS (
   SELECT user_id AS member_id,
+         is_eligibility_verified,
+         account_creation_date,
+         member_email,
          funnel_step_name,
          derived_tstamp,
          account_creation_date,
          is_mobile_device,
          client_id,
          client_name,
+         client_size,
+         client_industry,
          client_launch_date,
          eligibility_verification_method,
          page_urlpath,
          page_referrer,
-         row_number() over (partition by member_id order by derived_tstamp) AS step_order,
-
+         row_number() over (partition by member_id order by derived_tstamp) AS step_order
     FROM {{ ref('member_events') }}
    WHERE funnel_name = 'MEMBER_INTAKE'
 ),
@@ -23,12 +27,17 @@ WITH funnel_events AS (
 )
 
 SELECT funnel_events.member_id,
+       funnel_events.member_email,
+       funnel_events.is_eligibility_verified,
        funnel_events.client_id,
        funnel_events.client_name,
        funnel_events.client_launch_date,
        funnel_events.client_size,
+       funnel_events.client_industry,
+       funnel_events.eligibility_verification_method,
        funnel_events.step_name AS most_recent_step_completed,
        funnel_events.step_order AS most_recent_step_order_completed,
+       funnel_events.is_mobile_device,
        funnel_events.derived_tstamp AS most_recent_step_completed_at,
        funnel_events.page_urlpath AS most_recent_page_url,
        funnel_events.page_referrer AS previous_page_url,
